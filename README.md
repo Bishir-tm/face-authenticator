72d3d484af9bf1106156af1c827a5e5d2cf69f53
Bishir-tm <bishirtijjani247@gmail.com>
2024-04-30 12:10:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# face-authenticator
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Bishir-tm/face-authenticator)
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB Atlas
+mongoose
+  .connect(
+    'mongodb+srv://bitmo24:QWERTYqwerty123456!@users.saxeg6g.mongodb.net/?retryWrites=true&w=majority&appName=users',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+     }
+  )
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('Error connecting to MongoDB Atlas:', err));
+
+// Define schema for user data
+const userSchema = new mongoose.Schema({
+  name: String,
+  landmarks: [
+    {
+      x: Number,
+      y: Number,
+    },
+  ],
+});
+
+// Create model from schema
+const User = mongoose.model('User', userSchema);
+
+// Endpoint to save user data
+app.post('/api/users', async (req, res) => {
+  try {
+    const { name, landmarks } = req.body;
+    const user = new User({ name, landmarks });
+    await user.save();
+    res.status(201).send('User data saved successfully');
+  } catch (err) {
+    console.error('Error saving user data:', err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+// Endpoint to retrieve user data
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.error('Error retrieving user data:', err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+// Start the server
+app.listen(port, () =>
+  console.log(`Server listening at http://localhost:${port}`)
+);
