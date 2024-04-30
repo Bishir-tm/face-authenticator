d2630a0efa075930e58f7cf8b14c2a2d6e814998
Bishir-tm <bishirtijjani247@gmail.com>
2024-04-30 15:28:24 +0100

    ...

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-app.use(express.json());
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 // Connect to MongoDB Atlas
 mongoose
   .connect(
